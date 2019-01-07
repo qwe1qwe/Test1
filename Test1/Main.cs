8a2016a1ce6c6a1d76cb3816b402307f2bbc3af1
qwe1qwe <julian.stadler97@gmail.com>
2019-01-07 21:39:57 +0100

    initial

---
+++ Test1/Main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace Test1 {
+    public class Main : Script {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart() {
+            NAPI.Util.ConsoleOutput("Example resource loaded!");
+        }
+    }
+}
