6f8be32707b82a275a3cdbb27edbafc9811df977
OptimusCrime98 <simon.schwaiger98@gmail.com>
2019-01-07 22:05:36 +0100

    Added second arrow

--- Test1/Main.cs
+++ Test1/Main.cs
@@ -5,7 +5,7 @@ namespace Test1 {
     public class Main : Script {
         [ServerEvent(Event.ResourceStart)]
         public void OnResourceStart() {
-            NAPI.Util.ConsoleOutput("-> Example resource loaded!");
+            NAPI.Util.ConsoleOutput("-> -> Example resource loaded!");
         }
     }
 }
